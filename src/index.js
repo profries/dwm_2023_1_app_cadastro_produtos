532ffd03d51802357c39a604b9d83282390924d6
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-05-27 12:15:39 -0300

    Criacao das rotas

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import FormProdutos from './componentes/FormProdutos';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <FormProdutos />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="cadastro" element={<FormProdutos />} />
+    </Routes>
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
